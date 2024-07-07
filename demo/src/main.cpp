06fb6bffc0269430ec27f2d1fee26a12b3d8ee82
tengym <tengyingmian@willfine.com>
2024-07-07 10:02:06 +0800

    g711转aac应用修改

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -19,8 +19,71 @@ static char g_audio_file[128];
 static char g_muxer_file[128];
 
 static int TestG711ToAAC(char *audio_name)
+{
+    int active = 0;
+    InitParam initParam;
+    initParam.u32AudioSamplerate=8000;
+    initParam.ucAudioChannel=1;
+    initParam.u32PCMBitSize=16;
+    initParam.ucAudioCodec = Law_ALaw;
+    //initParam.ucAudioCodec = Law_ULaw;
 
-    while (1) sleep(1);
+    Mt_Handle handle = Mt_AACEncoder_Init(initParam);
+    char* infilename = (char *)audio_name;
+    char* outAacname = (char *)"audio.aac";
+
+    FILE* fpIn = fopen(infilename, "rb");
+    if(NULL == fpIn)
+    {
+        printf("%s:[%d] open %s file failed\n",__FUNCTION__,__LINE__,infilename);
+        return -1;
+    }
+
+    FILE* fpOut = fopen(outAacname, "wb");
+    if(NULL == fpOut)
+    {
+        printf("%s:[%d] open %s file failed\n",__FUNCTION__,__LINE__,outAacname);
+        return -1;
+    }
+
+    int gBytesRead = 0;
+    int bG711ABufferSize = 250;
+    int bAACBufferSize = 4*bG711ABufferSize;//提供足够大的缓冲区
+    unsigned char *pbG711ABuffer = (unsigned char *)malloc(bG711ABufferSize *sizeof(unsigned char));
+    unsigned char *pbAACBuffer = (unsigned char*)malloc(bAACBufferSize * sizeof(unsigned char));  
+    unsigned int out_len = 0;
+	unsigned long long ts = 0;
+
+    FrameInfo_t pframe = {0};
+    int count = 0;
+    while((gBytesRead = fread(pbG711ABuffer, 1, bG711ABufferSize, fpIn)) >0)
+    {    
+        if(Mt_AACEncoder_Encode(handle, pbG711ABuffer, gBytesRead, pbAACBuffer, &out_len) > 0)
+        {
+            fwrite(pbAACBuffer, 1, out_len, fpOut);
+ 
+            pframe.frame_head.data_len = out_len;
+            pframe.pdata = pbAACBuffer;
+            mem_put_frame(MEM_NAME_AUDIO, &pframe);
+            count++;
+            usleep(1000*1000/15);
+        }
+
+		ts += 1000000/25;
+        //usleep(1000*1000/25);
+    }
+    printf("aud_count:%d.\n", count);
+
+    Mt_AACEncoder_Release(handle);
+
+    free(pbG711ABuffer);
+    free(pbAACBuffer);
+    fclose(fpIn);
+    fclose(fpOut);
+
+    return 0;
+}
+
 
 static void *test_video(void*)
 {
